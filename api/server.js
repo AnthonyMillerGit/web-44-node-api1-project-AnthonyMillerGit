258bb21daec02df9590177111b00886f43691cd4
ferengitooth <ehitsamill@gmail.com>
2021-08-02 20:15:13 -0700

    succesful get request

--- api/server.js
+++ api/server.js
@@ -2,6 +2,22 @@
 const express = require('express')
 const server = express()
 server.use(express.json())
+const User = require('./users/model')
+const {json} = require('body-parser')
 
+// get
+server.get('/api/users', (req,res) => {
+    User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: `no users found`,
+            message: err.message,
+            stack: err.stack
+        })
+    })
+})
 
 module.exports = server
