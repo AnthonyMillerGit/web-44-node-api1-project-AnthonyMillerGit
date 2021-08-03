d162d4293534d0615a020040282942d7e1ade2b0
ferengitooth <ehitsamill@gmail.com>
2021-08-02 20:31:19 -0700

    succesful post and get by id

--- api/server.js
+++ api/server.js
@@ -20,4 +20,50 @@ server.get('/api/users', (req,res) => {
     })
 })
 
+// post
+
+server.post('/api/users', (req,res) => {
+    const newUser = req.body
+    User.insert(newUser)
+    .then(addUser => {
+        if(!newUser.name || !newUser.bio) {
+            res.status(400).json({
+                message: `provide name and bio`
+            })
+        } else {
+            const createUser = (newUser)
+            res.status(201).json(createUser)
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: `whelp`,
+            message: err.message,
+            stack: err.stack
+        })
+    })
+})
+
+// get ID
+server.get('/api/users/:id', (req,res) => {
+    User.findById(req.params.id)
+    .then(foundUser => {
+        if (!foundUser) {
+            res.status(404).json({
+                message: `does not exist`
+            })
+        } else {
+            res.json(foundUser)
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: `no users found`,
+            message: err.message,
+            stack: err.stack
+        })
+    })
+})
+
+
 module.exports = server
