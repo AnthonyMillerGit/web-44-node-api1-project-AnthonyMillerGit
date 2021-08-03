b609513719be56804872c0ce2141ac8105ccc20f
ferengitooth <ehitsamill@gmail.com>
2021-08-02 18:59:14 -0700

    hooked up server to index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const server = require('./api/server');
-
+console.log("Hello World!")
 const port = 5000;
 
 // START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log("listening on port 5000")
+})
+
